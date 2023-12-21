d90851d7f648c1c13f6a37038f5f889e4c636220
thiwanka778 <thiwankaudumulla@gmail.com>
2023-12-21 21:38:55 +0530

    update base url

--- src/redux/apiService.ts
+++ src/redux/apiService.ts
@@ -1,2 +1,2 @@
-export const  BASE_URL: string='http://localhost:8080'
-// export const BASE_URL:string = 'http://51.20.131.126:8080'
+// export const  BASE_URL: string='http://localhost:8080'
+export const BASE_URL:string = 'https://next-store-dev.online';
